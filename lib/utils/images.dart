6a0a6dc3c195b8e08040b52a5591069ec97c0688
Jayfablead <121211943+Jayfablead@users.noreply.github.com>
2025-09-03 17:36:15 +0530

    added things

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,4 +1,7 @@
 class Imgs {
-  static const String defaultprofile = 'assets/images/defaultprofile.png';
-  static const String defaultimage = 'assets/images/defaultimage.png';
+  static const String defaultProfile = 'assets/images/defaults/defaultProfile.jpg';
+  static const String defaultImage = 'assets/images/defaults/defaultImage.png';
+  static const String appLogo = 'assets/images/appLogo.png';
+  static const String onlyLogo = 'assets/images/onlyLogo.png';
+
 }
